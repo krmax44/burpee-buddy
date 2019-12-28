e53d1a8a198504b0d02f5204c9063258b4e5bd3f
adrco <adrcotfas@gmail.com>
2019-12-28 18:19:16 +0200

    Goals: edit default goals

--- app/src/main/java/com/apps/adrcotfas/burpeebuddy/db/goals/GoalFactory.java
+++ app/src/main/java/com/apps/adrcotfas/burpeebuddy/db/goals/GoalFactory.java
@@ -8,7 +8,7 @@ public class GoalFactory {
     public static List<Goal> getDefaultGoals() {
         List<Goal> goals = new ArrayList<>();
 
-        goals.add(Goal.getRepBasedGoal(3, 10, 30));
+        goals.add(Goal.getRepBasedGoal(3, 5, 30));
         goals.add(Goal.getRepBasedGoal(4, 15, 30));
         goals.add(Goal.getRepBasedGoal(5, 20, 30));
 
