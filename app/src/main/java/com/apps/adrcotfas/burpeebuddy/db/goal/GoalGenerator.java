d0ae8f28162733d2e0285eb65303ac2eca84185c
adrco <adrcotfas@gmail.com>
2020-02-23 20:25:56 +0200

    Goals Update default goals

--- app/src/main/java/com/apps/adrcotfas/burpeebuddy/db/goal/GoalGenerator.java
+++ app/src/main/java/com/apps/adrcotfas/burpeebuddy/db/goal/GoalGenerator.java
@@ -8,10 +8,9 @@ public class GoalGenerator {
     public static List<Goal> getDefaultGoals() {
         List<Goal> goals = new ArrayList<>();
 
-        goals.add(Goal.getRepBasedGoal(1, 30, 30));
-        goals.add(Goal.getRepBasedGoal(3, 5, 30));
-        goals.add(Goal.getRepBasedGoal(4, 15, 30));
-        goals.add(Goal.getRepBasedGoal(5, 20, 30));
+        goals.add(Goal.getRepBasedGoal(3, 15, 30));
+        goals.add(Goal.getRepBasedGoal(4, 20, 30));
+        goals.add(Goal.getRepBasedGoal(5, 15, 30));
 
         goals.add(Goal.getTimeBasedGoal(3, 30, 30));
         goals.add(Goal.getTimeBasedGoal(3, 60, 30));
