8aebb5c8ff3679484dc3a6e282d90cd01b450852
adrco <adrcotfas@gmail.com>
2020-03-07 22:33:49 +0200

    Update ExerciseGenerator

--- app/src/main/java/com/apps/adrcotfas/burpeebuddy/db/exercise/ExerciseGenerator.java
+++ app/src/main/java/com/apps/adrcotfas/burpeebuddy/db/exercise/ExerciseGenerator.java
@@ -10,12 +10,12 @@ public class ExerciseGenerator {
         workouts.add(new Exercise("burpees", ExerciseType.COUNTABLE, false));
         workouts.add(new Exercise("push-ups", ExerciseType.COUNTABLE, false));
         workouts.add(new Exercise("plank", ExerciseType.TIME_BASED, false));
+        workouts.add(new Exercise("sit-ups", ExerciseType.UNCOUNTABLE, false));
         workouts.add(new Exercise("pull-ups", ExerciseType.UNCOUNTABLE, false));
         workouts.add(new Exercise("dips", ExerciseType.UNCOUNTABLE, false));
         workouts.add(new Exercise("squats", ExerciseType.UNCOUNTABLE, false));
         workouts.add(new Exercise("kettlebell swings", ExerciseType.UNCOUNTABLE, false));
         workouts.add(new Exercise("dumbbell curls", ExerciseType.UNCOUNTABLE, false));
-        workouts.add(new Exercise("single-unders", ExerciseType.UNCOUNTABLE, false));
         workouts.add(new Exercise("double-unders", ExerciseType.UNCOUNTABLE, false));
 
         return workouts;
